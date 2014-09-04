4affbee4c57c2bcc1ea3b5910b21de93748e3a97
Agree <agreea@hotmail.com>
2014-09-04 10:35:09 +0300

    Added Google Play Services, implemented location  services

--- android/src/main/java/com/sc/mtaasafi/android/HomeScreen.java
+++ android/src/main/java/com/sc/mtaasafi/android/HomeScreen.java
@@ -2,11 +2,16 @@ package com.sc.mtaasafi.android;
 
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +19,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -31,8 +41,143 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class HomeScreen extends FragmentActivity {
+public class HomeScreen extends FragmentActivity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
     private MainFragment mainFragment;
+    private LocationClient mLocationClient;
+    private Location mCurrentLocation;
+
+
+    public static class ErrorDialogFragment extends DialogFragment {
+        private Dialog mDialog;
+        public ErrorDialogFragment(){
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+
+    // Google Play Services Setup:
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 15000;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "Connected to Google Play", Toast.LENGTH_SHORT).show();
+        mCurrentLocation = mLocationClient.getLastLocation();
+        Toast toast = Toast.makeText(this, "Location: " + mCurrentLocation.getLatitude()
+                + " " + mCurrentLocation.getLongitude(), Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onDisconnected() {
+        Toast.makeText(this, "Disconnected from Google Play. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            CharSequence text = "Google play connection failed, no resolution";
+            Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        // If activity launched was trying to resolve the connection
+        Log.w("SERVICES", "Activity result called");
+        switch (requestCode) {
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+                // and it resolved the connection
+                if (resultCode == Activity.RESULT_OK) {
+                        Log.w("SERVICES", "Yo the activity was okay--try reconnecting");
+                        break;
+                }
+                else{
+                    Log.w("SERVICES", "Activity result was NOT okay");
+                }
+        }
+    }
+
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.
+                        isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates",
+                    "Google Play services is available.");
+            return true;
+
+            // Google Play services was not available for some reason.
+            // resultCode holds the error code.
+        } else {
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                    resultCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                showErrorFragment(errorDialog);
+            }
+            return false;
+        }
+    }
+
+    private void showErrorFragment(Dialog errorDialog){
+        ErrorDialogFragment errorFragment =
+                new ErrorDialogFragment();
+        // Set the dialog in the DialogFragment
+        errorFragment.setDialog(errorDialog);
+        // Show the error dialog in the DialogFragment
+        errorFragment.show(getSupportFragmentManager(),
+                "Location Updates");
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +192,7 @@ public class HomeScreen extends FragmentActivity {
             mainFragment = (MainFragment) getSupportFragmentManager()
                     .findFragmentById(android.R.id.content);
         }
-
+        mLocationClient = new LocationClient(this, this, this);
         new HttpAsyncTask().execute("http://mtaasafi.spatialcollective.com/get_posts");
     }
 
@@ -85,6 +230,29 @@ public class HomeScreen extends FragmentActivity {
         return networkInfo != null && networkInfo.isConnected();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        String locationProviders = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if (locationProviders == null || locationProviders.equals("")) {
+            // TODO: show a dialog fragment that will say you need to turn on location to make this thing work
+            // If they say yes, send them to Location Settings
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+        }
+            // Connect the client.
+        mLocationClient.connect();
+    }
+
+    @Override
+    protected void onStop(){
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+
     private class HttpAsyncTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls){
@@ -99,6 +267,7 @@ public class HomeScreen extends FragmentActivity {
                 int len = jsonArray.length();
 
                 final List<String> listContent = new ArrayList<String>(len);
+                Log.d("onPostExecute", "retrieved content list of length: " + len);
                 for(int i = 0; i<len; i++){
                     try{
                         String message = jsonArray.getJSONObject(i).getString("content");
@@ -117,6 +286,7 @@ public class HomeScreen extends FragmentActivity {
                         startActivity(intent);
                     }
                 });
+                Log.d("onPostExecute","Set listView listener");
                 listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_expandable_list_item_1, listContent));
 
             }catch (JSONException e){
