318463c3fd6acc47c6adb46c47b2c34e0f03ab06
Agree <agreea@hotmail.com>
2014-11-14 20:23:11 +0300

    First commit

---
+++ android/src/main/java/com/sc/mtaasafi/android/SystemUtils/URLs.java
@@ -0,0 +1,9 @@
+package com.sc.mtaasafi.android.SystemUtils;
+
+/**
+ * Created by Agree on 11/14/2014.
+ */
+public class URLs {
+    public static final String FEED = "http://app.spatialcollective.com/mtaasafi/dev/fetch_reports/";
+    public static final String BASE_WRITE = "http://app.spatialcollective.com/mtaasafi/dev/add_post";
+}
---
+++ android/src/main/java/com/sc/mtaasafi/android/uploading/UploadingPic.java
@@ -0,0 +1,7 @@
+package com.sc.mtaasafi.android.uploading;
+
+/**
+ * Created by Agree on 11/14/2014.
+ */
+public class UploadingPic {
+}
