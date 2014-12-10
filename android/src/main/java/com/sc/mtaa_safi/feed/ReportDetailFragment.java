246e2937fb14fcf4c6f956584e6f6d1c4e21716d
David Kutalek <davkutalek@gmail.com>
2014-12-10 09:51:03 +0300

    Fixes to timestamp

--- android/src/main/java/com/sc/mtaa_safi/feed/ReportDetailFragment.java
+++ android/src/main/java/com/sc/mtaa_safi/feed/ReportDetailFragment.java
@@ -137,7 +137,7 @@ public class ReportDetailFragment extends Fragment implements LoaderManager.Load
         ((MainActivity) getActivity()).getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
     }
     private String createHumanReadableTimestamp() {
-        return new SimpleDateFormat("MMM dd 'yy at H:mm")
+        return new SimpleDateFormat("MMM dd' '''yy' at 'HH:mm")
                 .format(new java.util.Date(mReport.timeStamp));
     }
 
