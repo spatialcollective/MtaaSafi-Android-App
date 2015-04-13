833b7fa76ed59781d093e265ae84e9f9a54fbf44
kutalek <davkutalek@gmail.com>
2015-04-13 17:03:16 +0300

    updated url and json to match what django-gcm expects

--- android/src/main/java/com/sc/mtaa_safi/SystemUtils/RegisterWithGcm.java
+++ android/src/main/java/com/sc/mtaa_safi/SystemUtils/RegisterWithGcm.java
@@ -43,8 +43,9 @@ public class RegisterWithGcm extends AsyncTask<String, String, String> {
 
     private void sendRegistrationIdToBackend() throws JSONException, IOException {
         JSONObject json = new JSONObject();
-        json.put(Utils.PROPERTY_REG_ID, this.regid);
-        json.put("userId", Utils.getUserId(mContext));
+        json.put("reg_id", this.regid);
+        json.put("name", Utils.getUserName(mContext));
+        json.put("dev_id", "1234");
         JSONObject response = NetworkUtils.makeRequest(mContext.getString(R.string.registerId), "post", json);
         if (response != null && response.has("error") && response.getInt("error") > 400)
             cancel(true);
