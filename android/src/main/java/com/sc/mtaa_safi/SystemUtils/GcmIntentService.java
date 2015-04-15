09feb25d6192c8d5c2f2cf92ef4b89fd9e771507
ishuah91 <ishuah91@gmail.com>
2015-04-15 15:32:37 +0300

    notification changes

--- android/src/main/java/com/sc/mtaa_safi/SystemUtils/GcmIntentService.java
+++ android/src/main/java/com/sc/mtaa_safi/SystemUtils/GcmIntentService.java
@@ -14,6 +14,9 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.sc.mtaa_safi.R;
 import com.sc.mtaa_safi.feed.MainActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class GcmIntentService extends IntentService {
     private static final int NOTIFICATION_ID = 1;
     private NotificationManager mNotificationManager;
@@ -28,26 +31,30 @@ public class GcmIntentService extends IntentService {
 
         if (!extras.isEmpty()) { // has effect of unparcelling Bundle
             if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType))
-                sendNotification(extras.getString("msg"));
+                try {
+                    sendNotification(extras.getString("msg"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 //            else if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType))
 //            else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType))
         }
         GcmBroadcastReceiver.completeWakefulIntent(intent); // Release the wake lock
     }
 
-    private void sendNotification(String msg) {
+    private void sendNotification(String msg) throws JSONException {
         mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
+        JSONObject msg_data = new JSONObject(msg);
         Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("reportId", 33049);
+        intent.putExtra("reportId", msg_data.getInt("reportId"));
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder mBuilder =  new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_logo)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-                .setContentTitle(getResources().getString(R.string.app_name))
-                .setContentText(msg);
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg_data.getString("blurb")))
+                .setContentTitle(msg_data.getString("message"))
+                .setContentText(msg_data.getString("blurb"));
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
